f760bbcc7143daa71299a829258dea10a355dd16
Nick <devwasik@gmail.com>
2022-03-23 14:29:17 -0500

    Update tankQueries.js

--- pages/api/db/tankQueries.js
+++ pages/api/db/tankQueries.js
@@ -2,7 +2,7 @@ import { db } from "./dbConfig";
 
 const createTank = (name, size, type, location) => {
   return db.query(
-    `INSERT INTO Tank(UserID, Name, Size, Type, Location) VALUES(1, $1, $2, $3, $4)`,
+    `INSERT INTO "Tank"("UserID", "Name", "Size", "Type", "Location") VALUES(1, $1, $2, $3, $4)`,
     [name, size, type, location]
   );
 };
